5d1e7230b56218fe16a0a1161497f6406142a97d
FrancescoCialfiDev <cialfifrancesco02@gmail.com>
2024-10-29 16:46:44 +0100

    Init create for iterazione

---
+++ js/script.js
@@ -0,0 +1,26 @@
+
+// Applichiamo controllo avanzato - ristretto del codice
+"use strict"
+
+// Effettuiamo una pulizia auotmatica della console
+console.clear();
+
+// Apertura di un Iterazione
+// Inizializzazione variabile - Condizione - Incremento
+let numeri;
+
+for (let numeri = 1; numeri <= 100; numeri = numeri++ ) {
+    console.log(numeri);
+    
+    
+    if(numeri % 3 === 0 && numeri % 5 === 0 ) {
+        console.log("FizzBuzz");
+    }  else if (numeri % 3 === 0){
+        console.log("Fizz");
+    } else if (numeri % 5 === 0){
+        console.log("Buzz");
+    }  
+    
+}
+
+
